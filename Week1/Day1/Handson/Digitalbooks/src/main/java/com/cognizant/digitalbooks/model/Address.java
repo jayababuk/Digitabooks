575d9d3835e0ad8fa9e61ccd4e5514171ab0efe5
jayababu <ketha.jayababu@gmail.com>
2022-09-11 15:11:05 +0530

    week1 day1 changes

---
+++ Week1/Day1/Handson/Digitalbooks/src/main/java/com/cognizant/digitalbooks/model/Address.java
@@ -0,0 +1,12 @@
+package com.cognizant.digitalbooks.model;
+
+public class Address {
+	private String city;
+	public void setcity(String city) {
+		this.city=city;
+	}
+	public String getCity() {
+		return this.city;
+	}
+
+}
